b2cc211953bb28335e198398294849ec15864534
Xalamander01 <aleksandarstanchev@abv.bg>
2023-03-23 22:12:34 +0200

    Finished with GUI

--- src/main/java/org/businessLogic/GUI.java
+++ src/main/java/org/businessLogic/GUI.java
@@ -1,101 +1,116 @@
 package org.businessLogic;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
+import javax.swing.table.AbstractTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class GUI implements ActionListener {
 
-    static JLabel fileNameLabel;
+    static JLabel fileNameLabel = new JLabel((String) null);
+    static JFrame windowFrame;
+    static JScrollPane scrollPane = new JScrollPane(null);
 
     public GUI() {
     }
 
     public static void renderUI() {
 
+        /*
+         * create a new window titled "Collaborations UI"
+         * add a button and attach an action listener to it
+         * add a label just after the button, which will show the path to the chosen file
+         */
+
         GUI gui = new GUI();
 
-        JFrame windowFrame = new JFrame("Collaborations UI");
+        windowFrame = new JFrame("Collaborations UI");
         windowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        windowFrame.setSize(600, 550);
-        windowFrame.setVisible(true);
+        windowFrame.setSize(800, 550);
 
         JButton button1 = new JButton("Open CSV");
         button1.addActionListener(gui);
-        JPanel fileChooserPanel = new JPanel();
-        fileChooserPanel.add(button1);
-        fileNameLabel = new JLabel((String) null);
-        fileChooserPanel.add(fileNameLabel);
 
-        JPanel tablePanel = new JPanel();
-        String[] colNames = new String[]{"Employee ID #1", "Employee ID #2", "Project ID", "Days worked"};
-        String[][] tableData = getTableData(fileNameLabel.getText());
-        JTable table = new JTable(tableData, colNames);
-        table.setBounds(30, 40, 200, 300);
-        DefaultTableModel tableModel = (DefaultTableModel) table.getModel();
-        JScrollPane scrollPane = new JScrollPane(table);
-        tablePanel.add(scrollPane);
-        //tablePanel.add(processTableData(fileNameLabel.getText()));
+        windowFrame.getContentPane().add(BorderLayout.NORTH, button1);
+        windowFrame.getContentPane().add(BorderLayout.CENTER, fileNameLabel);
+        //windowFrame.getContentPane().add(BorderLayout.SOUTH, scrollPane);
 
-        windowFrame.getContentPane().add(BorderLayout.NORTH, fileChooserPanel);
-        windowFrame.getContentPane().add(BorderLayout.SOUTH, tablePanel);
+        windowFrame.setVisible(true);
     }
 
-    private static JScrollPane processTableData(String filePath) {
-        String[] colNames = new String[]{"Employee ID #1", "Employee ID #2", "Project ID", "Days worked"};
-        String[][] tableData = getTableData(filePath);
-        JTable table = new JTable(tableData, colNames);
-        table.setBounds(30, 40, 200, 300);
-        JScrollPane scrollPane = new JScrollPane(table);
-        return scrollPane;
-    }
-
-    private static String[][] getTableData(String filePath) {
-        /*
-         * take the desired file path and
-         * return a 2d array which can be displayed
-         * in a table
-         */
-        String[][] tableData = new String[0][];
-        if (filePath != null) {
-            Map<Integer, List<EmployeeIdAndDates>> projectAndEmployees = EmployeeIdAndDates.parseCSVFile(filePath);
-            Map<Integer, Collaboration> collaborationsMap = Collaboration.getCollaborationsMap(projectAndEmployees);
-
-            List<String[]> list = new ArrayList<>();
-
-            for (Integer projectId : collaborationsMap.keySet()) {
-                Collaboration currentCollab = collaborationsMap.get(projectId);
-                String[] tableRow = {String.valueOf(currentCollab.getFirstEmployeeId()),
-                        String.valueOf(currentCollab.getSecondEmployeeId()),
-                        String.valueOf(projectId),
-                        String.valueOf(currentCollab.getDaysTogether())};
-                list.add(tableRow);
-            }
-            tableData = list.toArray(new String[0][]);
-        }
-        return tableData;
-    }
-
+    @Override
     public void actionPerformed(ActionEvent e) {
 
         /*
-         * if the user has successfully selected a new csv file
-         * update the file name label
-         * otherwise do nothing
+         * when triggered, if the "Open CSV" button is pressed, open a file chooser window
+         * if the user has successfully selected a file
+         * update the label, populate the table model and render it in the window
          */
-        if (e.getActionCommand() == "Open CSV") {
+        if (e.getActionCommand().equals("Open CSV")) {
 
-            JFileChooser j = new JFileChooser("src/test/csv");
+            JFileChooser fileChooser = new JFileChooser("src/test/csv");
 
-            int r = j.showSaveDialog(null);
-            if (r == JFileChooser.APPROVE_OPTION) {
-                fileNameLabel.setText(j.getSelectedFile().getAbsolutePath());
+            int state = fileChooser.showSaveDialog(null);
+            if (state == JFileChooser.APPROVE_OPTION) {
+                fileNameLabel.setText(fileChooser.getSelectedFile().getAbsolutePath());
+                FileTableModel fileTableModel = new FileTableModel(fileChooser.getSelectedFile().getAbsolutePath());
+                JTable table = new JTable(fileTableModel);
+                windowFrame.remove(scrollPane);
+                scrollPane = new JScrollPane(table);
+                windowFrame.getContentPane().add(BorderLayout.SOUTH, scrollPane);
             }
+        }
+    }
+
+    /*
+     * Create an abstract table model to hold the data which will be displayed in the table
+     */
+    static class FileTableModel extends AbstractTableModel {
+        protected String filePath;
+        protected Map<Integer, Collaboration> collaborationsMap;
+        protected String[] columnNames = new String[]{"Employee ID #1", "Employee ID #2", "Project ID", "Days worked"};
+        protected Class[] columnClasses = new Class[]{Integer.class, Integer.class, Integer.class, Long.class};
+
+        public FileTableModel(String filePath) {
+            this.filePath = filePath;
+            Map<Integer, List<EmployeeIdAndDates>> projectAndEmployees = EmployeeIdAndDates.parseCSVFile(filePath);
+            this.collaborationsMap = Collaboration.getCollaborationsMap(projectAndEmployees);
+        }
+
+        public int getColumnCount() {
+            return columnNames.length;
+        } // this is 4 always
+
+        public int getRowCount() {
+            return this.collaborationsMap.size(); // this changes depending on the number of projects in the csv file
+        }
+
+        public String getColumnName(int col) {
+            return columnNames[col];
+        }
+
+        public Class getColumnClass(int col) {
+            return columnClasses[col];
+        }
+
+        // set the value of each cell accordingly
+        public Object getValueAt(int row, int col) {
+            Integer projectId = (Integer) collaborationsMap.keySet().toArray()[row];
+            Collaboration collaboration = collaborationsMap.get(projectId);
+            return switch (col) {
+                case 0 -> collaboration.getFirstEmployeeId();
+                case 1 -> collaboration.getSecondEmployeeId();
+                case 2 -> projectId;
+                case 3 -> collaboration.getDaysTogether();
+                default -> null;
+            };
+        }
+
+        public boolean isCellEditable(int row, int col) {
+            return true;
         }
     }
 }
--- src/main/java/org/businessLogic/Main.java
+++ src/main/java/org/businessLogic/Main.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         /*
-        Map<Integer, List<EmployeeIdAndDates>> projectAndEmployees = EmployeeIdAndDates.parseCSVFile("src/test/csv/SampleData.csv");
+        Map<Integer, List<EmployeeIdAndDates>> projectAndEmployees = EmployeeIdAndDates.parseCSVFile("src/test/csv/SampleData2.csv");
         Map<Integer,Collaboration> collaborationsMap = Collaboration.getCollaborationsMap(projectAndEmployees);
         for ( Integer projectId : collaborationsMap.keySet() ) {
             System.out.println(collaborationsMap.get(projectId));
         }
-
          */
+
         GUI.renderUI();
     }
 }
