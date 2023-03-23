ae39c063a00f7cdc229b529e39ab0c9c33169153
AlexStan <109579607+Xalamander01@users.noreply.github.com>
2023-03-23 08:37:25 +0200

    This program now works without GUIs

--- src/main/java/org/businessLogic/Main.java
+++ src/main/java/org/businessLogic/Main.java
@@ -5,14 +5,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        /*
         Map<Integer, List<EmployeeIdAndDates>> projectAndEmployees = EmployeeIdAndDates.parseCSVFile("src/test/csv/SampleData.csv");
         Map<Integer,Collaboration> collaborationsMap = Collaboration.getCollaborationsMap(projectAndEmployees);
         for ( Integer projectId : collaborationsMap.keySet() ) {
             System.out.println(collaborationsMap.get(projectId));
         }
-
-         */
-        GUI.renderUI("src/test/csv/SampleData.csv");
+        //GUI.renderUI("src/test/csv/SampleData.csv");
     }
 }
